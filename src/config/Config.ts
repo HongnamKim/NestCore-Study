4879ccd410354e96dc41e641a516021f8cd4e3dd
HongnamKim <ghdskawkd@naver.com>
2024-10-21 17:18:46 +0900

    Nest-MSA 시작

--- src/config/Config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-import * as process from 'process';
-import { ENV_HOST_KEY } from '../common/const/env-keys.const';
-
-export default registerAs('test', () => ({
-  host: process.env[ENV_HOST_KEY] ?? 'localhost',
-  port: 3000,
-}));
--- src/posts/comments/dto/update-comments.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommentsDto } from './create-comments.dto';
-
-export class UpdateCommentsDto extends PartialType<CreateCommentsDto>(
-  CreateCommentsDto,
-) {}
